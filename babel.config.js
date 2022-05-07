b540b79dbb45cda2be61209e77e18120e0ba46c2
kkam678 <43460084+kkam678@users.noreply.github.com>
2022-05-07 13:23:26 +0900

    babel 설정 에러 버그 수정

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     presets: ['module:metro-react-native-babel-preset'],
     plugins: [
+        //TypeError: undefined is not an object (evaluating 'this._callListeners.bind')
+        //에러 날때 '@babel/plugin-transform-flow-strip-types' 추가하여 해결함
+        //react-native 의 babel 설정과 달라서 그럴수도 있는데 이럴때는 react-native run-android --reset-cache 캐시 삭제해줌
+        ['@babel/plugin-transform-flow-strip-types'],
         ["@babel/plugin-proposal-decorators", {"legacy": true}],
         ["@babel/plugin-proposal-class-properties", {"loose": true}]
     ]
